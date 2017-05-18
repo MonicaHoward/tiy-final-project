3e2da0322768cb864ddfc27ef4e3421204d86791
Monica Howard <jay.m.howard@gmail.com>
2017-05-18 14:25:35 -0500

    added right section which is copy of left side created movie search files to hold state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 // COMPONENTS
 import Header from './Header.js';
 import Sidebar from './Sidebar.js';
+import MovieSearch from './MovieSearch.js';
 import LeftQuery from './LeftQuery.js'
+import RightQuery from './RightQuery.js';
 import Footer from './Footer.js';
-
-// EXTRAS
+// CSS & EXTRAS
 import './App.css';
 
 class App extends Component {
   render() {
     return  (
       <div>
-        <Header/>
+        <Header />
         <Sidebar />
+        <MovieSearch />
         <LeftQuery />
+        <RightQuery />
+        <MovieSearch />
         <Footer />
       </div>
     );
---
+++ src/RightQuery.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RightQuery extends React.Component {
+
+  render() {
+    return (
+      <section className="duel-query">
+        <div className="main-content">
+          <input type="text" className="search-query" />
+        </div>
+      </section>
+    );
+  }
+}
+module.exports = RightQuery;
