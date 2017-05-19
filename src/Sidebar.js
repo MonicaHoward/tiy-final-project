8cc97aa81e8c364b13be709677cd606ac071ea01
Monica Howard <jay.m.howard@gmail.com>
2017-05-19 12:35:19 -0500

    Initializing State

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,7 +6,7 @@ class Sidebar extends React.Component {
       <section className="side-bar">
         <h3><a href="MovieDuel.js">MOVIE DUEL</a></h3>
         <h3><a href="#">MOVIE RANDOMIZER</a></h3>
-        <h3><a href="MovieSearch.js">MOVIE SEARCH</a></h3>
+        <h3><a href="./MovieSearch.js">MOVIE SEARCH</a></h3>
       </section>
     );
   }
