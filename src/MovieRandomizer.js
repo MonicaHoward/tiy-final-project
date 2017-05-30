a50440dfa71ef799a3cc4ce362ef58951422bf26
Monica Howard <jay.m.howard@gmail.com>
2017-05-30 15:09:11 -0500

    nothing major

--- src/MovieRandomizer.js
+++ src/MovieRandomizer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 
-const url = `https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=`;
+
 
 class MovieRandomizer extends React.Component {
 
@@ -10,21 +10,25 @@ class MovieRandomizer extends React.Component {
     this.state = {
       allMovieData: [],
       randomMovie: [],
-      movieId: Math.ceil(Math.random() * 300)
+      movieId: ''
     }
   }
   randomizer() {
-    
+    this.setState ({
+      randomMovie: Math.ceil(Math.random() * 9000)
+
+    })
+
   }
 
   handleClick(evt) {
       $.ajax({
-        url:  `https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=${this.state.randomMovie}`,
+        url:  `https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=goodfellas`,
       })
 
       .done((data) => {
         this.setState({
-          randomMovie: data.results.movieId
+          allMovieData: data.results
         })
         console.log("hellooooo data",data);
       });
