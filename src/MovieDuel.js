960dd03c4ffbaa0b8431676d0cdebaca3fa38d14
Monica Howard <jay.m.howard@gmail.com>
2017-05-31 19:45:18 -0500

    start over on MovieDuel js logic

--- src/MovieDuel.js
+++ src/MovieDuel.js
@@ -5,31 +5,16 @@ class MovieDuel extends React.Component {
   constructor() {
     super();
     this.state = {
-      opponentOne: '',
-      opponentTwo: ''
+
     }
   }
 
 
-  handleClick(evt) {
-
-  }
 
   render() {
-    console.log('*what is????!?!?!?!?!?*', this.props.displayThis)
+
     return (
-      <section className="movie-duel">
-        <div className="duel-outcome">
-          <h1>The Winner Is: </h1>
-        </div>
-
-        <button className="lets-duel"
-          onClick={(evt) => this.handleClick(evt)}>
-          <h1>Let's Duel</h1>
-        </button>
-
-      </section>
-
+    
     )
   }
 
