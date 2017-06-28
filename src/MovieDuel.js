5c5adc2cf8077890e03da2af74ce66b56e3e371a
Monica Howard <jay.m.howard@gmail.com>
2017-06-27 22:44:45 -0500

    its getting better

--- src/MovieDuel.js
+++ src/MovieDuel.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MovieSearch from './MovieSearch.js';
 import $ from 'jquery';
 
+const url = `https://image.tmdb.org/t/p/w500`;
+
 class MovieDuel extends React.Component {
   constructor() {
     super();
@@ -31,12 +33,29 @@ class MovieDuel extends React.Component {
   }
 
   render() {
-
+    let displayThis;
+    if (this.state.allMovieData.length > 0) {
+      displayThis = this.state.allMovieData.map((movie) => {
+        return (
+          <ul className="search-results" key={this.state.smovie.id}>
+            <div className="box-header">
+            </div>
+            <li className="movie-poster"><img src={`${url}${this.state.movie.poster_path}`} alt={this.this.state.movie.title} /></li>
+            <div className="movie-info">
+              <li className="movie-title">{this.state.movie.title}</li>
+              <li>Movie Rating: {this.state.movie.vote_average}</li>
+              <li>Synopsis: {this.state.movie.overview.split('.', 1)}</li>
+            </div>
+          </ul>
+        )
+      });
+    }
 
     return (
       <div>
         <div className="movie-duel-search">
-          <MovieSearch />
+          <MovieSearch
+          {displayThis}/>
         </div>
 
         <div className="movie-duel-search">
